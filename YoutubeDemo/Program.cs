6046069e624965ff096380468a6b1597baeefc8a
Berker ERTAN <65434879+berkerertan@users.noreply.github.com>
2024-02-09 10:06:59 +0300

    Add files via upload

---
+++ YoutubeDemo/Program.cs
@@ -0,0 +1,121 @@
+﻿using System.Net.Http.Headers;
+//BERKER
+class program
+{
+    static void Main(string[] args)
+    {
+        //IoC Container
+        CustomerManager customerManager = new CustomerManager(new Customer(), new TeacherCreditManager());
+        customerManager.GiveCredit();
+
+
+        Console.ReadLine();
+    }
+
+    class CreditManager
+    {
+        public void Calculate(int creditType)
+        {
+            
+
+            Console.WriteLine("Hesaplandı");
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Kredi Verildi");
+        }
+    }
+    
+    interface ICreditManager
+    {
+        void Calculate();
+        void Save();
+    }
+    abstract class BaseCreditManager : ICreditManager
+    {
+        public abstract void Calculate();
+
+        public virtual void Save()
+        {
+            Console.WriteLine("Kaydedildi");
+        }
+
+
+    }
+    class TeacherCreditManager : BaseCreditManager, ICreditManager
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Öğretmen Kredisi Hesaplandı");
+        }
+        public virtual void Save()
+        {
+            base.Save();
+        }
+    }
+
+    class MilitaryCreditManager : BaseCreditManager, ICreditManager
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Asker Kredisi Hesaplandı");
+        }
+        class CarCreditManager : BaseCreditManager, ICreditManager
+        {
+            public override void Calculate()
+            {
+                Console.WriteLine("Araba Kredisi Hesaplandı");
+            }
+        }
+
+    }
+    //SOLID
+    class Customer
+    {
+        public Customer()
+        {
+            Console.WriteLine("Müşteri nesnesi başlatıldı");
+        }
+        public int Id { get; set; }
+
+        public string City { get; set; }
+
+    }
+    class Company : Customer
+    {
+        public string CompanyName { get; set; }
+        public string TaxNumber { get; set; }
+    }
+
+    class Person : Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NationalIdentity { get; set; }
+    }
+    //Katmanlı Mimariler
+    class CustomerManager
+    {
+        private Customer _customer;
+        private ICreditManager _creditManager;
+        public CustomerManager(Customer customer, ICreditManager creditManager)
+        {
+            _customer = customer;
+            _creditManager = creditManager;
+        }
+        public void Save()
+        {
+            Console.WriteLine("Müşteri Kaydedildi");
+        }
+        public void Delete()
+        {
+            Console.WriteLine("Müşteri Silindi");
+        }
+        public void GiveCredit()
+        {
+            _creditManager.Calculate();
+            Console.WriteLine("Kredim verildi");
+        }
+    }
+}
